6efe5945ff2c7b45176af2f9d6e996c513fe78f9
den-taku <dentaku1412@gmail.com>
2022-08-29 23:58:34 +0900

    Read up to 4

--- src/data.rs
+++ src/data.rs
@@ -2,18 +2,29 @@
 //!
 //! Druid uses `Data` to know when it should re-render
 
-use druid::{im::Vector, Data, Lens};
+use druid::{im::Vector, Data, Env, EventCtx, Lens};
 
 #[derive(Clone, Data, Lens)]
 pub struct AppState {
+    new_todo: String,
     todos: Vector<TodoItem>,
 }
 
 impl AppState {
     pub fn new(todos: Vec<TodoItem>) -> Self {
         Self {
+            new_todo: "".into(),
             todos: Vector::from(todos),
         }
+    }
+
+    fn add_todo(&mut self) {
+        self.todos.push_front(TodoItem::new(&self.new_todo));
+        self.new_todo = "".into();
+    }
+
+    pub fn click_add(_ctx: &mut EventCtx, data: &mut Self, _env: &Env) {
+        data.add_todo()
     }
 }
 
--- src/view.rs
+++ src/view.rs
@@ -2,7 +2,7 @@
 
 use crate::data::*;
 use druid::{
-    widget::{Checkbox, Flex, Label, List},
+    widget::{Button, Checkbox, Flex, Label, List, TextBox},
     Widget, WidgetExt,
 };
 
@@ -13,6 +13,22 @@ fn todo_item() -> impl Widget<TodoItem> {
     Flex::row().with_child(checkbox).with_flex_child(label, 1.)
 }
 
+fn new_todo_textbox() -> impl Widget<AppState> {
+    let new_todo_textbox = TextBox::new()
+        .with_placeholder("Add a new todo")
+        .expand_width()
+        .lens(AppState::new_todo);
+
+    // let add_todo_button = Button::new("Add").on_click(|_ctx: &mut EventCtx, data: &mut AppState, _env: &Env| data.add_todo());
+    let add_todo_button = Button::new("Add").on_click(AppState::click_add);
+
+    Flex::row()
+        .with_flex_child(new_todo_textbox, 1.)
+        .with_child(add_todo_button)
+}
+
 pub fn build_ui() -> impl Widget<AppState> {
-    List::new(todo_item).lens(AppState::todos)
+    Flex::column()
+        .with_child(new_todo_textbox())
+        .with_child(List::new(todo_item).lens(AppState::todos))
 }
